e164b42ac78b12e9a1fd201f4ea167057b57845f
kjy <tilehouse0225@naver.com>
2021-06-21 19:42:45 +0900

    pythonanywhere 사이트 배포수정

--- bdb.py
+++ bdb.py
@@ -1,9 +1,9 @@
 import pymysql
 
 def insert_data(email, pw):
-    db = pymysql.connect(host='127.0.0.1',
-                    user='root', password='1234',
-                    db='bbdb', charset='utf8')
+    db = pymysql.connect(host='bemyluv1.mysql.pythonanywhere-services.com',
+                    user='bemyluv1', password='kk2563159@',
+                    db='bemyluv1$mydb', charset='utf8')
     c = db.cursor()
     setdata = (email, pw)
     c.execute("INSERT INTO user_tb VALUES (%s, %s)", setdata)
